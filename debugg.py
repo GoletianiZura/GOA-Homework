61977662ad9f8b0e3e0feb6eaa00a93d9ca365f9
GoletianiZura <159282740+GoletianiZura@users.noreply.github.com>
2024-02-15 20:13:47 +0400

    Add files via upload

---
+++ debugg.py
@@ -0,0 +1,48 @@
+#wrong
+color -> "red"
+#right
+color = red
+
+#Wrong
+salary = 50000
+role = Analyst
+age -> 29
+print(salary)
+
+#Right
+salary = 50000
+role = "Analyst"
+age = 29
+print(salary)
+
+#wrong (snake case)
+Dog-Goldie
+
+#right (snake_case)
+Dog_goldie
+
+#Wrong
+DeviceType = iPad
+print(Device_Type)
+
+#Right
+Device_type = ipad
+print(Device_type)
+
+#wrong
+salary = 1000
+pay_raise = 100
+print(new_salary)
+new_salary = salary + pay_raise
+
+#right
+salary = 1000
+pay_raise = 100
+new_salary = salary + pay_raise
+print(new_salary)
+
+#wrong
+dog name : "Goofy"
+
+#right
+dog_name = "Goofy"
