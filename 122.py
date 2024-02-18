b830da3bcc1cf69be03e7dad65439c297ce3f813
GoletianiZura <159282740+GoletianiZura@users.noreply.github.com>
2024-02-18 15:58:09 +0400

    Day 0/

---
+++ 122.py
@@ -0,0 +1,11 @@
+#Variables
+name = "zura"
+surname = "goletiani"
+age = 26
+height = 1,73
+I_know = False
+
+
+#print(type(("I'm " + name + " " + surname + ", I'm " + str(age) + " years old and I'm ",height, "m tall. Fact about me: I know programming, right computer? " + str(I_know) + ".")))
+
+print(min(age,height))
