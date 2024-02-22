d38ed132be7a3b6aee30d55b86865ed73027e0ee
GoletianiZura <159282740+GoletianiZura@users.noreply.github.com>
2024-02-22 16:13:00 +0400

    Add files via upload

---
+++ Day 08/Homework 8.py
@@ -0,0 +1,29 @@
+# for loop'ით და while loop'ით ეცადეთ გამოიტანოთ რიცხვები 1'დან 100-ის ჩათვლით
+for i in range(100):
+    print(i + 1)
+print("-----")
+
+i = 0
+while i <= 100:
+    print(i)
+    i = i + 1
+print("-----")
+
+
+# for ციკლის მეშვეობით შეკრიბეთ პირველი 10 რიცხვი
+
+sum_of_digits = 0
+for i in range(11):
+    sum_of_digits += i
+print(sum_of_digits)
+
+
+print("-----")
+
+# for ციკლის მეშვეობით გამოიტანეთ ლუწი რიცხვები 1'დან 20'ის ჩათვლით
+
+for i in range(20):
+    print(i + 1)
+    i = i + 1
+sum_of_digits = 0
+print("-----")
