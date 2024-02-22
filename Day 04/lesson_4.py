3f9bf4ec91a509f721c0e5c5c0f00f89eb07075d
GoletianiZura <159282740+GoletianiZura@users.noreply.github.com>
2024-02-22 16:12:40 +0400

    Add files via upload

---
+++ Day 04/lesson_4.py
@@ -0,0 +1,15 @@
+#fam age
+dad = 53
+mom = 46
+me = 26
+lil_bro_1 = 24
+lil_bro_2 = 24 #twins
+lil_bro_3 = 14
+sis= 8
+age = 2
+
+print(dad + age. mom + age. me + age. lil_bro_1 + age. lil_bro_2 + age. lil_bro_3 + age. sis + age)
+
+
+
+
