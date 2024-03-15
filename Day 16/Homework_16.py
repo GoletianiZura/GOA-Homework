08bdea94859f2021f0a74fab43fa650ba52c6acf
GoletianiZura <159282740+GoletianiZura@users.noreply.github.com>
2024-03-15 21:55:12 +0400

    Add files via upload

---
+++ Day 16/Homework_16.py
@@ -0,0 +1,12 @@
+print("Here is what will streets do to you, if you dont comply.")
+
+boy_ask= input("რა ტიპი ხარ? ")
+
+if boy_ask== "ტიპი":
+  print("კარგად დაფიქრდი!")
+elif boy_ask== "კაი ტიპი" or boy_ask== "კარგი ტიპი":
+  print("მოდი ვიძმაკაცოთ")
+elif boy_ask== "ძველი ბიჭი":
+  print("ფული! ჩქარა!!")
+else:
+  print("ახლა იცემები")
---
+++ Day 17/homework_17.py
@@ -0,0 +1,78 @@
+# Write a function that takes a list of numbers as input and returns the sum of all the numbers in the list.
+
+# nums_list= [1,5,6,7,15,20]
+
+# def func(calc):
+#   return sum(calc)
+
+# print(func(nums_list))
+
+
+# Write a function that takes a list of strings as input and returns a new list containing only the strings that have a length greater than 5.
+
+# str_list= ["kobe","james","garnett","wilkins","pierce"]
+
+# def func(appending):
+#   new_list = []
+#   for x in appending:
+#     if len(x) > 5:
+#       new_list.append(x)
+#   return new_list
+
+# print(func(str_list))
+
+# Write a function that takes a list of numbers as input and returns a new list containing only the even numbers from the original list.
+
+# nums_list=[24,37,28,35,21]
+
+# def evens(nums):
+#   new_list= []
+#   for x in nums:
+#     if x % 2 == 0:
+#       new_list.append(x)
+#   return new_list
+
+# print(evens(nums_list))
+
+# Write a function that takes a list of numbers as input and returns the largest number in the list.
+
+# def listinput(inputa):
+#   return max(inputa)
+
+# print(listinput(input("enter nums with commas: ")))
+
+# Write a function that takes a list of strings as input and returns a new list containing only the strings that start with the letter 'a'.
+
+# not done
+
+# def stringus(randous):
+#   new_str = []
+#   for i in randous:
+#     if i.startswith("a"): new_str.append(i)
+#   return new_str
+# print(stringus(input("anything:")))
+
+# Write a function that takes a list of numbers as input and returns a new list containing the square of each number.
+
+# def squares(ofnum):
+#   new_sqr=[]
+#   for i in ofnum.split(","):
+#     new_sqr.append(int(i)*int(i))
+#   return new_sqr
+
+# print(squares(int(input("write random nums: "))))
+
+# Write a function that takes a list of strings as input and returns a new list containing the lengths of each string.
+# str_list= ["kobe","james","garnett","wilkins","pierce"]
+
+# def func(stringus):
+#   new_str =[]
+#   for string in stringus.split():
+#     new_str.append(len(string.strip()))
+
+#   return new_str
+
+# print(func(input("enter string for list: ")))
+  
+
+# Write a function that takes a list of numbers as input and returns the sum of all the numbers that are greater than 10.
